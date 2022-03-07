6587917c43ba943d1839e61c3ac4b89624009c80
huzitaiwo <oyeniyi4real@gmail.com>
2022-03-07 17:00:44 +0100

    movie component

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ const Movie = ({movie}) => {
     return ( 
         <div className="movie">
             <h2>{ movie.title}</h2>
-            <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt="" />
+            <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt={movie.title} />
         </div>
     );
 }
