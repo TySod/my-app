45866421d4edc1d46994d04d42eae5a36bc0b3f9
huzitaiwo <oyeniyi4real@gmail.com>
2022-03-07 17:02:26 +0100

    working on the filter component

---
+++ src/Filter.js
@@ -0,0 +1,11 @@
+const Filter = () => {
+    return (
+        <div className="filter">
+            <button>All</button>
+            <button>Comedy</button>
+            <button>Action</button>
+        </div>
+    );
+}
+ 
+export default Filter;
